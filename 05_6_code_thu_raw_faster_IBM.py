74eb1c55329ecfbc2a274dca584944f058895e1e
Brett Melbourne <brett.melbourne@colorado.edu>
2026-09-17 23:03:42 -0600

    Update raw code (but still provisional) for week 5 Thu

--- 05_6_code_thu_raw_faster_IBM.py
+++ 05_6_code_thu_raw_faster_IBM.py
@@ -8,42 +8,42 @@ from time import time
 # Factor=2 (doubling size) by default
 # Suggested fills for various dtypes
 #     dtype    fill
-#    -----------------
-#     float    np.nan
+#    ----------------------
+#     float    0 or np.nan
 #     int      -1
 #     bool     False
 
 def grow(x, fill, factor=2):
 
+    # get original array size
     n = len(x)
 
+    # make new bigger array (double by default)
     x_new = np.full(factor * n, fill, dtype=x.dtype)
+
+    # copy original array into new one
     x_new[:n] = x
 
     return x_new
+
+
 
 # Simulation control
 rng = np.random.default_rng(95756)
 t = 0.0      #needs to be float, e.g 0.0
 t_max = 100
-# isize = 20   #initial array size for individuals, must be > N_0 below
-# psize = 20   #initial array size for population info, > 1
+
 
 # Initial biological state
 
 # --individual scale (parameters to set)
 N_0 = 10
-# alive = init(np.full(N_0, True), isize, False)  # alive state
-# b = init(np.full(N_0, 0.25), isize, 0)          # birth rate
-# m = init(np.full(N_0, 0.2), isize, 0)           # death rate
 alive = np.full(N_0, True)  # alive state
 b = np.full(N_0, 0.25)      # birth rate
 m = np.full(N_0, 0.2)       # death rate
 
 # --population scale (automatic)
 N_t = np.sum(alive) #scale transition (sum over individuals -> population)
-# N = init(np.full(1, N_t), psize, -1)          #track N
-# times = init(np.full(1, t), psize, np.nan)    #track time
 N = np.array([N_t])     #track N
 times = np.array([t])   #track time
 
@@ -53,6 +53,7 @@ total_death = np.sum(m * alive)
 intensity = total_birth + total_death
 
 # Grow arrays to make room for new data
+
 alive = grow(alive, fill=False, factor=100)
 b = grow(b, fill=0, factor=100)
 m = grow(m, fill=0, factor=100)
@@ -63,13 +64,15 @@ times = grow(times, fill=np.nan, factor=1000)
 psize = len(N)
 
 # Simulate events
+
 k = 0        #count number of events
 irow = N_0   #array row we insert the next individual info into
 prow = 1     #array row we insert the next population info into
 chkpt_t = 1
-#while t < t_max and N_t > 0 and irow < isize and prow < psize:
+
 while t < t_max and N_t > 0:
 
+    # grow arrays if needed
     if irow == isize:
         alive = grow(alive, 0)
         b = grow(b, 0)
@@ -81,7 +84,7 @@ while t < t_max and N_t > 0:
         times = grow(times, np.nan)
         psize = psize * 2
 
-    start_time = time()
+    start_time = time() #timer
 
     # waiting time to next event
     w = rng.exponential(scale=1/intensity)
@@ -97,43 +100,46 @@ while t < t_max and N_t > 0:
 
     # update system state (do event)
     if event_birth:
-        # add a new individual and its traits
-        # alive = np.append(alive, 1)
-        # b = np.append(b, 0.25)
-        # m = np.append(m, 0.2)
+        # insert new individual in arrays
         alive[irow] = 1
         b[irow] = b[i]  #inherit from parent
         m[irow] = m[i]
+        # update intensities
         total_birth = total_birth + b[i]
         total_death = total_death + m[i]
+        # update N
         N_t = N_t + 1
+        # increment row index for next time
         irow += 1
     else:
         # death
         alive[i] = 0
+        # update intensities
         total_birth = total_birth - b[i]
         total_death = total_death - m[i]
+        # update N
         N_t = N_t - 1
 
     intensity = total_birth + total_death
 
-    # N = np.append(N, N_t)
-    # times = np.append(times, t)
+    # insert population level states in arrays
     N[prow] = N_t
     times[prow] = t
+    # increment row index for next time
     prow += 1
 
-    end_time = time()
+    end_time = time() #stop timer
 
-    k += 1
+    k += 1 #event counter
 
+    # some monitoring at integer time checkpoints
     if t > chkpt_t:
         tt = end_time - start_time
         print(f"N:{N_t} L:{len(alive)} t:{t:.1f} Evt:{k} Comp_t: {tt}")
         chkpt_t += 1
 
 
-# Trim arrays
+# Trim arrays (remove the unused elements)
 irow = irow - 1
 alive = alive[:irow]
 b = b[:irow]
@@ -142,6 +148,8 @@ m = m[:irow]
 prow = prow - 1
 N = N[:prow]
 times = times[:prow]
+
+# Now we have clean arrays as before and can see results
 
 print(alive)
 
